ac6df74965c1800b5c04fe7c5da1054b266ac0dc
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2025-05-12 13:34:27 +0300

    First tested version of Student class

--- 5/student/test_student.cpp
+++ 5/student/test_student.cpp
@@ -2,24 +2,27 @@
 
 int main()
 {
+    cout << "Test 1: Default ctor and output" << endl;
     Student s;
     cout << "Printing new student: " << endl;
     cout << s << endl << endl;
 
+    cout << "Test 2: Input / Output" << endl;
     cout << "Enter student information: (id, first, last, up to 5 courses) ";
     cin >> s;
     cout << "Printing updated student information: " << endl;
     cout << s << endl << endl;
 
-    Student st(322667270, "Matt", "Kuperwasser");
-    cout << "Printing new student: " << endl;
-    cout << st << endl << endl;
-
-    cout << "Testing [] operator: " << endl;
+    cout << "Test 3: [] operator: " << endl;
     int course;
     cout << "Enter course id (1-5): " << endl;
     cin >> course;
-    cout << (st[course] ? "Signed up" : "Not Signed Up") << endl;
+    cout << (s[course] ? "Signed up" : "Not Signed Up") << endl;
+
+    cout << "Test 4: Assignment ctor" << endl;
+    Student st(322667270, "Matt", "Kuperwasser");
+    cout << "Printing new student: " << endl;
+    cout << st << endl << endl;
 
     return 0;
 }
