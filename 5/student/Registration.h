601753600f91040090847341250a9939bf027d46
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2025-05-12 13:41:07 +0300

    started working on Registration class in pset 5

--- 5/student/Registration.h
+++ 5/student/Registration.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <fstream>
+#include <string>
 #include "Student.h"
 
 class Registration
 {
 private:
+    fstream* fileObj;
 
 public:
-
+    // ctor
+    Registration(const string&);
+    // dtor
+    ~Registration();
+    bool createFile(const string&);
+    Student readStudent(const int&);
+    void writeStudent(const Student&);
 };
