a67eb620ead3588827e20f4afe82188150eef69e
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2025-06-06 09:23:52 +0300

    initializations and ctors

--- 8/Student.cpp
+++ 8/Student.cpp
@@ -1,1 +1,21 @@
 #include "Student.h"
+
+
+// Method to print student information and scholarship elegibility
+void Student::print() const
+{
+    if (this->milga())
+        cout << "Scholarship" << endl;
+    
+    cout << "ID: " << this->id << endl
+         << "NAME: " << this->firstName << " "
+         << this->lastName << endl;
+}
+
+
+// Method to get student information from the user
+void Student::input()
+{
+    cout << "enter id, first name, last name, number of courses" << endl;
+    cin >> this->id >> this->firstName >> this->lastName >> this->numberOfCourses;
+}
--- 8/Student.h
+++ 8/Student.h
@@ -1,6 +1,31 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Student
 {
+protected:
+    int id;
+    string firstName;
+    string lastName;
+    int numberOfCourses;
 
+public:
+    // Default ctor
+    Student(int id = 0, string fn = "", string ln = "", int numCourses = 0);
+    // Copy ctor
+    Student(const Student&);
+    // Virtual dtor
+    virtual ~Student();
+
+    // Abstract method to compute whether the student is eligible for scholarship
+    virtual bool milga() const = 0;
+    // Abstract method that returns the student type (BA / MA / PHD)
+    virtual string studType() const = 0;
+    // Method to print student information and scholarship elegibility
+    virtual void print() const;
+    // Method to get student information from the user
+    virtual void input();
 };
